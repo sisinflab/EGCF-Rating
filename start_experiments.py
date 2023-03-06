d13af5586d4be9abb4fce0235bedb314aa5a24d0
dmalitesta <d.malitesta@gmail.com>
2023-03-06 09:26:13 +0100

    GCN added.

--- start_experiments.py
+++ start_experiments.py
@@ -2,7 +2,7 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
-parser.add_argument('--config', type=str, default='test_error')
+parser.add_argument('--config', type=str, default='test_error_baselines_seeds')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.config}.yml")
